06dc9f11dc7ec06554cae16357c1211cec417ecd
Jake Greer <jakegreer11@gmail.com>
2017-09-15 17:23:53 -0700

    re-worked the form and css

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,43 +1,78 @@
-var friends = [{
-        name: "Jake Greer",
-        img: "https://www.jakegreer.net",
-        scores: [
-            4,
-            5,
-            1,
-            2,
-            4,
-            3,
-            2,
-            1,
-            5,
-            4,
-            3,
-            2
-        ]
-    },
-    {
-        name: "Billy Surch",
-        img: "https://www.billysurch.net",
-        scores: [
-            4,
-            5,
-            1,
-            2,
-            4,
-            3,
-            2,
-            1,
-            5,
-            4,
-            3,
-            2
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    var newFriend = {
+        name: $("#nameInput").val().trim(),
+        link: $("#imageLink").val().trim(),
+        scores: [$("#q-one"), $("#q-two"), $("#q-three"), $("#q-four"), $("#q-five"), $("#q-six"),
+            $("#q-seven"), $("#q-eight"), $("#q-nine"), $("#q-ten"), $("#q-eleven"), $("#q-twelve"), $("#q-thirteen")
         ]
     }
 
-];
+    $.post("/api/new", newFriend)
+        .done(function(data) {
+            console.log(data);
+            alert("Adding you as a friend...");
+        });
+})
 
 
+//requires the node file system
+var fs = require("fs");
+
+//sets up a constructor that will build our users info
+var users = function(name, link, scores) {
+    this.name = name;
+    this.link = link;
+    this.scores = scores;
+}
+
+//this array will be used to hold the final organized data
+var friendsList = [];
+
+//calling the readFile method to take in the text file 'friends.txt'
+//returns an error if one occurs
+fs.readFile("app/data/friends.txt", "utf8", function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    //logs that the file is reading...
+    console.log("Reading from list of friends...");
+
+    //this splits the data in the text file by every comma and stores each compenent into one long arrray
+    var friends = data.split(",");
 
 
-module.exports = friends;
+    //this will be used for an index counter for our final friendsList array..this is not used as i inside the for loop because i will be incrementing at a different pace than needed. therefore a new index counter is made to hold the value needed.
+    var j = 0;
+
+    //for loop is created because the current data in the friends array is not in the format that is needed.
+    //this for loop is going to call a new users contstructor every itteration.
+    //15 values are inserted as arguments on every itteration this is because there is a 'name', 'link', and 'scores' that is an array of 13...in total 15 values are needed.
+    //the incrementer loops over every 15 elements because that is one user.
+    for (var i = 0; i < friends.length; i += 15) {
+        friendsList[j] = new users(
+            friends[i], //name
+            friends[i + 1], //link
+            [parseInt(friends[i + 3]), //scores
+                parseInt(friends[i + 4]),
+                parseInt(friends[i + 5]),
+                parseInt(friends[i + 6]),
+                parseInt(friends[i + 7]),
+                parseInt(friends[i + 8]),
+                parseInt(friends[i + 9]),
+                parseInt(friends[i + 10]),
+                parseInt(friends[i + 11]),
+                parseInt(friends[i + 12]),
+                parseInt(friends[i + 13]),
+                parseInt(friends[i + 14])
+            ]);
+
+        //loops the friendsList to the next index
+        j++;
+    }
+
+    console.log(friendsList);
+})
+
+//exports the final array
+exports.friendsList;
--- server.js
+++ server.js
@@ -24,4 +24,26 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Set up the users from the database
 /********************************************************************
  *******************************************************************/
-var friends = require("/data/friends.js");
+var friends = require("./app/data/friends.js");
+console.log(friends);
+
+
+//Routes the user to the starting page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+//Routes the user to the survey page
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.post("api/new", function(req, res) {
+    var newfriend = req.body;
+
+    console.log(newfriend);
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
